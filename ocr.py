6edb6b5c4556fea1df870a78ab6de997e4704082
Akib Mohammed Khan <akibmohammed@iut-dhaka.edu>
2021-03-06 21:48:10 +0600

    update ocr.py

--- ocr.py
+++ ocr.py
@@ -16,18 +16,12 @@ def detectText(img):
 
     response = client.document_text_detection(image=image)
     texts = response.text_annotations
-    print('Texts:')
-
-    for text in texts:
-        print('\n"{}"'.format(text.description))
-
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                for vertex in text.bounding_poly.vertices])
-
-        print('bounds: {}'.format(','.join(vertices)))
+    output=texts[0].description
 
     if response.error.message:
         raise Exception(
              '{}\nFor more info on error messages, check: '
              'https://cloud.google.com/apis/design/errors'.format(
                response.error.message))
+
+    return output
