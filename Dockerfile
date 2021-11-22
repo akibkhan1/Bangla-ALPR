bb5854c423ba1b39001636536706e1a27ce97eff
root <root@ip-172-31-21-160.ap-southeast-1.compute.internal>
2021-11-22 16:08:07 +0000

    update from server

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,6 @@ RUN apt-get install ffmpeg libsm6 libxext6  -y
 RUN mkdir /app
 WORKDIR /app
 COPY requirements.txt /app/
+RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . /app/
--- app.py
+++ app.py
@@ -164,4 +164,4 @@ def get_text(image_path):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=True)
